efa2b4b638032d4624aba0d1335ced2b676d06ac
Nikesh Ulak <cloudnikesh@gmail.com>
2018-10-09 17:46:05 +0545

    fullpage markup, js and css

---
+++ assets/custom/custom-fullpage.js
@@ -0,0 +1,65 @@
++( function( $ ) {
+	function fullpage_init() {		
+	    $('#content.site-content').fullpage({
+	        verticalCentered: true,
+	        scrollOverflow: true,
+
+	        licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+	        
+	        css3: false,
+	        anchors: [
+	        	'section1', 
+	        	'section2', 
+	        	'section3', 
+	        	'section4',  
+	        	'section5', 
+	        	'section6', 
+	        	'section7'
+	        	],
+	        menu: '#fp-menu',
+	    });
+
+        // calling fullpage.js methods using jQuery
+	    $('#moveSectionDown').click(function(e){
+	        e.preventDefault();
+	        $.fn.fullpage.moveSectionDown();
+	    });
+	}
+
+	function page_padding_top() {
+		var header_height = $('header').height();
+
+		if($('body.home').hasClass('fullpage-enabled')) {
+			header_height += 30;
+			$('body.home .section .evt-img-overlay').css({ 'padding-top': header_height + 'px' });
+		}
+	}
+
+	function fullpage_width() {
+		$('body.home .evt-img-overlay').width( $(window).width() );
+	}
+
+	$(window).resize(function() {
+		page_padding_top();
+
+		// fix the width
+		fullpage_width();
+	});
+
+	$(document).ready(function() {
+		$('body.home').addClass('fullpage-enabled');
+
+		// fullpage on homepage only
+		if($('body.home').hasClass('fullpage-enabled')) {
+			// initialize fullpage
+			fullpage_init();
+
+			// fix the width
+			fullpage_width();
+
+			// fix padding top
+			page_padding_top();
+		}
+	});
+
+} )( jQuery );
--- functions.php
+++ functions.php
@@ -8,7 +8,20 @@
 function full_click_style() {
 	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/assets/frameworks/bootstrap/bootstrap.css' );
 
+	if(is_front_page()) {
+		// fullpage for homepage only
+		wp_enqueue_style( 'fullpage-css', get_stylesheet_directory_uri() . '/assets/frameworks/fullpage/fullpage.css' );
+
+		wp_enqueue_script( 'jquery-scrolloverflow', get_stylesheet_directory_uri() . '/assets/frameworks/fullpage/scrolloverflow.js', array('jquery'), true );
+		wp_enqueue_script( 'jquery-fullpage', get_stylesheet_directory_uri() . '/assets/frameworks/fullpage/fullpage.js', array('jquery'), true );
+
+		wp_enqueue_script( 'business-click-custom-fullpage', get_stylesheet_directory_uri() . '/assets/custom/custom-fullpage.js', array('jquery'), true );
+	}
+
+	// parent theme style
 	wp_enqueue_style( 'business-click-style', get_template_directory_uri() . '/style.css' );
+	
+	// theme style
 	wp_enqueue_style( 'full-click-style',get_stylesheet_directory_uri() . '/style.css',array('business-click-style'));
 }
 add_action( 'wp_enqueue_scripts', 'full_click_style' );
